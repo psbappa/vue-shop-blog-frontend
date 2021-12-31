2068fb8b3775db8315cd4b9730dced4c7dc07a52
BappaD <bappa.dey@einzigtech.com>
2021-12-31 10:50:17 +0530

    [WIP] Added tasks component and its UI template

---
+++ src/components/test/Movies/Header.vue
@@ -0,0 +1,163 @@
+<template>
+    <div class="movies-header">
+        <!-- Header -->
+        <header role="banner">
+            <h1>Daily Schedule</h1>
+            <form class="task-form" @submit.prevent="submitProject">
+                <input type="text" v-model="addedNewTask" name="task" placeholder="Enter task name">
+                <input type="submit" value="Submit">
+            </form>
+        </header>
+        <!-- End header -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        data() {
+            return {
+                tasks: [],
+                counter: 1,
+            }
+        },
+        methods: {
+            submitProject() {
+                this.tasks.push({id: this.counter++ , workFor: this.addedNewTask, hours: {hh: '00', mm: '00'}, counter: '1' })
+                this.$emit('clicked', this.tasks)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+*:focus{
+  outline:none;
+}
+body {
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+  box-sizing: border-box;
+}
+
+p {
+  margin: 0;
+}
+button{
+  cursor:pointer;
+}
+
+button > * {
+  pointer-events: none;
+}
+
+main {
+  padding: 40px 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+@media screen and (min-width: 768px) {
+  main {
+    padding: 80px 30px;
+  }
+}
+
+header {
+  background: #4A00E0;
+  background: linear-gradient(to top, #4A00E0 0%, #e22d9a 100%);
+  padding: 40px 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+@media screen and (min-width: 768px) {
+  header {
+    padding: 80px 30px;
+  }
+}
+
+header h1 {
+  color: #fff;
+  font-weight: 300;
+  font-size: 2rem;
+  margin: 0 0 60px 0;
+  text-transform:uppercase;
+  letter-spacing:2px;
+}
+
+@media screen and (min-width: 768px) {
+  header h1 {
+    font-size: 2.25rem;
+  }
+}
+
+form {
+  text-align:right;
+  position:relative;
+  width: 75%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+@media screen and (min-width: 768px) {
+  form {
+    max-width: 650px;
+    flex-direction: row;
+  }
+}
+
+input[type="text"] {
+  position:absolute;
+  width: 100%;
+  border-color: transparent;
+  padding: 15px 20px;
+  border-radius: 30px;
+  margin-bottom: 20px;
+}
+
+@media screen and (min-width: 768px) {
+  input[type="text"] {
+    margin-bottom: 0;
+    margin-right: 20px;
+    padding: 20px 30px;
+  }
+}
+
+input[type="submit"] {
+  position:absolute;
+  right:-35px;
+  z-index:1;
+  border-color: transparent;
+  border-style: solid;
+  border-width: 2px;
+  width: 120px;
+  background: #00C1D9;
+  color: #fff;
+  border-radius: 30px;
+  padding: 15px;
+  text-transform: uppercase;
+  font-weight: 700;
+  transition: all 0.3s;
+  cursor:pointer;
+  border-color: #fff;
+}
+
+input[type="submit"]:hover {
+  background: #4A00E0;
+  border-color: #fff;
+}
+
+@media screen and (min-width: 768px) {
+  input[type="submit"] {
+    width: 150px;
+    padding: 20px;
+  }
+}
+</style>
--- src/components/test/Movies/Movies.vue
+++ src/components/test/Movies/Movies.vue
@@ -1,44 +1,66 @@
 <template>
     <div class="movies">
-        <!-- Header -->
-        <header role="banner">
-            <h1>Daily Schedule</h1>
-            <form class="project-form" @submit.prevent="submitProject">
-                <input type="text" name="project" placeholder="Enter project name">
-                <input type="submit" value="Submit">
-            </form>
-        </header>
-        <!-- End header -->
-
-        <!-- Main -->
-        <main role="main">
-            <ul class="projects">
-                <li id="project-0">
-                    <h2>Bappa</h2>
+      <div class="container1">
+        <Header @clicked="addProjectChild" />
+        <div class="row show-grid">
+          <div class="col-lg-8 col-lg-pull-4 col-md-8 col-md-pull-0 col-sm-8 col-sm-pull-4">
+            <!-- Main -->
+            <!-- <pre>{{ JSON.stringify(work, null, 2) }}</pre> -->
+            <main role="main">
+                <ul class="tasks">
+                  <div v-for="(item, index) in work" :key="index">
+                    <li id="task-0" style="width: 130%;">
+                    <h2><span class="text-warnign">{{ item.id }} . </span>{{ item.workFor }}</h2>
                     <div class="timer">
-                        <p class="timer-label">Total Time Spent</p>
-                        <p class="timer-text"><span class="hours">00</span>:<span class="minutes">00</span>:<span class="seconds">00</span></p>
+                      <p class="timer-label">Total time spend</p>
+                      <p class="timer-text"><span class="hours">{{ item.hours.hh }}</span>:<span class="minutes">{{ item.hours.mm }}</span></p>
                     </div>
-                    <button v-if="isShowStart" @click="toggleIsStartBtn(false)" class="btn stop">Stop</button>
-                    <button v-else @click="toggleIsStartBtn(true)" class="btn start">Start</button>
-                    <!-- <button class="delete-btn"><i class="fa fa-times"></i></button> -->
-                </li>
-            </ul>
-        </main>
-        <!-- End main-->
-
-
+                    <button v-if="isLogDataForm" @click="showLogDataForm({status: false, id: item.id})" class="btn stop">off</button>
+                    <button v-else @click="showLogDataForm({status: true, id: item.id})" class="btn start">Log Time</button>
+                    <button @click="removeItem(index)" class="delete-btn"><fa icon="trash" /></button>
+                  </li>
+                </div>
+                <span>total time:</span>
+                <strong><span class='text-primary'>{{totalTime}}/-</span></strong>
+                   
+                </ul>
+            </main>
+            <!-- End main-->
+          </div>
+          <div class="col-lg-4 col-lg-push-8 col-md-4 col-md-push-0 col-sm-4 col-sm-push-8">
+            <div v-if="isLogDataForm" class="log-data-form">
+              <h4>Log data</h4>
+              <form>
+                <button @click.prevent="saveTimeSpend">save</button>
+                <div class="">
+                  <label>Choose hours</label>
+                  <vue-timepicker :format="datTimeFormat" v-model="hoursForTask"></vue-timepicker>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div> 
+      </div>
     </div>
 </template>
 
 <script>
+    import VueTimepicker from 'vue3-timepicker/src/VueTimepicker.vue'
     import axios from 'axios'
+    import Header from '../Movies/Header.vue'
 
     export default {
         name: 'Movies',
+        components: {
+          VueTimepicker,
+          Header
+        },
         data() {
-            return {
-                isShowStart: false
+          return {
+            toggleLogFormData: false,
+              isLogDataForm: false,
+              work: [],
+              passedIdToForm: '',
             }
         },
         mounted () {
@@ -47,215 +69,136 @@
             // this.spotifyApi();
         },
         methods: {
-            toggleIsStartBtn(isShowStart) {
-                this.isShowStart = isShowStart
-            },
-            submitProject() {
-                console.log('Test...')
-            },
-            bitCoinData() {
-                axios
-                    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-                    .then(response => (
-                        console.log('Response: ', response.data.bpi)
-                    ))
-                    .catch(error => console.log(error))
-            },
-            utellyMovieApi() {
-                axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+          showLogDataForm(isLogDataForm) {
+            this.isLogDataForm = isLogDataForm.status
+            this.passedIdToForm = isLogDataForm.id
+          },
+          addProjectChild(value) {
+            this.work = value
+          },
+          removeItem(index) {
+            this.work.splice(index, 1);
+            // if (confirm('Sure to delete')) {
+            //     this.work = this.work.filter((item) => item.id != selData.id)
+            //     this.$toast.open({
+            //         message: "Deleted Successfully",
+            //         type: "warning",
+            //         duration: 5000,
+            //         dismissible: true
+            //     })
+            // }
+            console.log('Final Tasks: ', this.work)
+          },
+          saveTimeSpend() {
+            this.work.filter(item => {
+              if(item.id === this.passedIdToForm) {
+                item.hours.hh = this.hoursForTask.HH
+                item.hours.mm = this.hoursForTask.mm
+              }
+            })
 
-                var options = {
-                    method: 'GET',
-                    url: 'http://www.omdbapi.com/?i=tt3896198&apikey=952d00d0',
-                    // params: {term: 'bojack', country: 'uk'},
-                    headers: {
-                        // 'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
-                        // 'x-rapidapi-key': 'dc81d997dcmshe98c1d25b263992p1ea820jsn27e716468128'
-                        
-                        "Access-Control-Allow-Origin": "*",
-                    }
-                };
+            this.work.map(e => {
+              console.log('e', e.hours)
+            })
+          },
+          // Unused methods start---------
+          bitCoinData() {
+              axios
+                  .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                  .then(response => (
+                      console.log('Response: ', response.data.bpi)
+                  ))
+                  .catch(error => console.log(error))
+          },
+          utellyMovieApi() {
+              axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-                axios.request(options).then(function (response) {
-                    console.log('utellyMovieApi', response);
-                }).catch(function (error) {
-                    console.error('utellyMovieApi Error', error);
-                });
-            },
-            spotifyApi() {
-                var options = {
-                    method: 'GET',
-                    // url: 'https://spotifystefan-skliarovv1.p.rapidapi.com/listCategories',
-                    url: 'http://localhost:8080/',
-                    headers: {
-                        'content-type': 'application/x-www-form-urlencoded',
-                        'x-rapidapi-host': 'Spotifystefan-skliarovV1.p.rapidapi.com',
-                        'x-rapidapi-key': 'dc81d997dcmshe98c1d25b263992p1ea820jsn27e716468128'
-                    },
-                    data: {accessToken: '29946401b8b74433b52673e9a9fcc07a'}
-                    };
+              var options = {
+                  method: 'GET',
+                  url: 'http://www.omdbapi.com/?i=tt3896198&apikey=952d00d0',
+                  // params: {term: 'bojack', country: 'uk'},
+                  headers: {
+                      // 'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+                      // 'x-rapidapi-key': 'dc81d997dcmshe98c1d25b263992p1ea820jsn27e716468128'
+                      
+                      "Access-Control-Allow-Origin": "*",
+                  }
+              };
 
-                    axios.request(options).then(function (response) {
-                        console.log(response.data);
-                    }).catch(function (error) {
-                        console.error(error);
-                    });
-            },
+              axios.request(options).then(function (response) {
+                  console.log('utellyMovieApi', response);
+              }).catch(function (error) {
+                  console.error('utellyMovieApi Error', error);
+              });
+          },
+          spotifyApi() {
+              var options = {
+                  method: 'GET',
+                  // url: 'https://spotifystefan-skliarovv1.p.rapidapi.com/listCategories',
+                  url: 'http://localhost:8080/',
+                  headers: {
+                      'content-type': 'application/x-www-form-urlencoded',
+                      'x-rapidapi-host': 'Spotifystefan-skliarovV1.p.rapidapi.com',
+                      'x-rapidapi-key': 'dc81d997dcmshe98c1d25b263992p1ea820jsn27e716468128'
+                  },
+                  data: {accessToken: '29946401b8b74433b52673e9a9fcc07a'}
+                  };
+
+                  axios.request(options).then(function (response) {
+                      console.log(response.data);
+                  }).catch(function (error) {
+                      console.error(error);
+                  });
+          },
+            // Unused methods End---------
+        },
+        computed: {
+          totalTime() {
+            let total = 0
+            console.log('total: ', this.work)
+            // for (let i = 0; i < this.work.length; i++){
+            //     total += parseInt(this.expensesDataInTable[i].expenses);
+            // }
+            return total;
+          }
         }
     }
 </script>
 
 <style scoped>
 /* https://vuejs.org/v2/cookbook/using-axios-to-consume-apis.html */
-*:focus{
-  outline:none;
-}
-body {
-  font-family: "Open Sans", sans-serif;
-  font-size: 16px;
-  box-sizing: border-box;
-}
 
-p {
-  margin: 0;
+/* Left right section start */
+.show-grid [class^=col-] {
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background-color: #eee;
+    background-color: rgba(86,61,124,.15);
+    border: 1px solid #ddd;
+    border: 1px solid rgba(86,61,124,.2);
 }
-button{
-  cursor:pointer;
-}
+/* Left right section end */
 
-button > * {
-  pointer-events: none;
-}
 
-main {
-  padding: 40px 15px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-@media screen and (min-width: 768px) {
-  main {
-    padding: 80px 30px;
-  }
-}
-
-header {
-  background: #4A00E0;
-  background: linear-gradient(to top, #4A00E0 0%, #e22d9a 100%);
-  padding: 40px 15px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-@media screen and (min-width: 768px) {
-  header {
-    padding: 80px 30px;
-  }
-}
-
-header h1 {
-  color: #fff;
-  font-weight: 300;
-  font-size: 2rem;
-  margin: 0 0 60px 0;
-  text-transform:uppercase;
-  letter-spacing:2px;
-}
-
-@media screen and (min-width: 768px) {
-  header h1 {
-    font-size: 2.25rem;
-  }
-}
-
-form {
-  text-align:right;
-  position:relative;
-  width: 75%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-@media screen and (min-width: 768px) {
-  form {
-    max-width: 650px;
-    flex-direction: row;
-  }
-}
-
-input[type="text"] {
-  position:absolute;
-  width: 100%;
-  border-color: transparent;
-  padding: 15px 20px;
-  border-radius: 30px;
-  margin-bottom: 20px;
-}
-
-@media screen and (min-width: 768px) {
-  input[type="text"] {
-    margin-bottom: 0;
-    margin-right: 20px;
-    padding: 20px 30px;
-  }
-}
-
-input[type="submit"] {
-  position:absolute;
-  right:-35px;
-  z-index:1;
-  border-color: transparent;
-  border-style: solid;
-  border-width: 2px;
-  width: 120px;
-  background: #00C1D9;
-  color: #fff;
-  border-radius: 30px;
-  padding: 15px;
-  text-transform: uppercase;
-  font-weight: 700;
-  transition: all 0.3s;
-  cursor:pointer;
-  border-color: #fff;
-}
-
-input[type="submit"]:hover {
-  background: #4A00E0;
-  border-color: #fff;
-}
-
-@media screen and (min-width: 768px) {
-  input[type="submit"] {
-    width: 150px;
-    padding: 20px;
-  }
-}
-
-.projects {
+.tasks {
   list-style: none;
   padding: 0;
   width: 100%;
 }
 
 @media screen and (min-width: 768px) {
-  .projects {
+  .tasks {
     max-width: 80%;
   }
 }
 
 @media screen and (min-width: 991px) {
-  .projects {
+  .tasks {
     max-width: 60%;
   }
 }
 
-.projects li {
+.tasks li {
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
   border-radius: 15px;
   padding: 20px;
@@ -268,20 +211,20 @@ input[type="submit"]:hover {
   overflow:hidden;
 }
 
-.projects li h2 {
+.tasks li h2 {
   font-size: 1.125rem;
   color: #4A4A4A;
 }
 
 @media screen and (min-width: 768px) {
-  .projects li h2 {
+  .tasks li h2 {
     font-size: 1.313rem;
     margin-right: auto;
   }
 }
 
 @media screen and (min-width: 768px) {
-  .projects li {
+  .tasks li {
     padding: 25px;
     flex-direction: row;
     justify-content: flex-end;
@@ -356,6 +299,11 @@ input[type="submit"]:hover {
   border-color: #21D324;
   border-style: solid;
 }
+.stop:hover {
+  color: #d32121;
+  border-color: #d32121;
+  border-style: solid;
+}
 
 .stop {
   background: #D00202;
@@ -378,15 +326,15 @@ input[type="submit"]:hover {
   bottom: 25px;
   right: 30px;
   color: #fff;
-  display: flex;
+  /* display: flex; */
   justify-content: center;
   align-content: center;
   transition: background 0.3s, opacity 0.3s, transform 0.5s;
 }
 
-.delete-btn i {
+/* .delete-btn i {
   font-size: 1.5rem;
-}
+} */
 
 .delete-btn:hover {
   color: #D00202;
@@ -403,21 +351,21 @@ input[type="submit"]:hover {
   }
 }
 
-.projects li:hover .delete-btn,
-.projects li:hover .timer,
-.projects li:hover .btn {
+.tasks li:hover .delete-btn,
+.tasks li:hover .timer,
+.tasks li:hover .btn {
   opacity: 1;
 }
 
 @media screen and (min-width: 768px) {
-  .projects li:hover .delete-btn,
-  .projects li:hover .timer,
-  .projects li:hover .btn {
+  .tasks li:hover .delete-btn,
+  .tasks li:hover .timer,
+  .tasks li:hover .btn {
     transform: translateX(0);
   }
 }
 
-.projects input {
+.tasks input {
   width: 100%;
   padding: 0.5rem;
   font-size: 1.125rem;
@@ -428,7 +376,7 @@ input[type="submit"]:hover {
 }
 
 @media screen and (min-width: 768px) {
-  .projects input {
+  .tasks input {
     width: 50%;
     font-size: 1.313rem;
     margin-bottom: 0;
